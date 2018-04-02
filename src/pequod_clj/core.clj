f5655513836a9750dcaf8c4e867fca8cdfb74c23
Mitchell Szczepanczyk <mitchell@szcz.org>
2018-04-01 19:21:15 -0500

    Generate WCs

--- src/pequod_clj/core.clj
+++ src/pequod_clj/core.clj
@@ -31,12 +31,37 @@
         pdlist (repeat (+ finals input resources labors) 0.05)]))
 
 
-(defn create-ccs [consumer-councils workers-per-council]
+(defn create-ccs [consumer-councils workers-per-council finals]
   (let [effort 1]
     (repeat consumer-councils 
             {:num-workers workers-per-council
              :effort effort
-             :income (* 500 effort workers-per-council)})))
+             :income (* 500 effort workers-per-council)
+             :cy (+ 6 (rand 9)) ; scalar for whole product in utility function
+             :final-demands (repeat 5 0)
+             :utility-exponents []
+             :cz (/ .5 finals)})))
+
+
+(defn create-wcs [worker-councils goods industry]
+   (flatten
+     (for [good goods]
+        (repeat (/ worker-councils 2 (count goods))
+                {:industry industry
+                 :product good}))))
+
+
+(defn setup-wcs [wc]
+  (letfn [(get-random-subset [input-seq]
+            (->> input-seq
+                 shuffle
+                 (take (rand-nth input-seq))
+                 sort))]
+   (let [l1 (get-random-subset intermediate-inputs)
+         l2 (get-random-subset nature-types)
+         l3 (get-random-subset labor-types)
+         production-inputs (vector l1 l2 l3)
+         ])))
 
 
 (defn setup []
@@ -49,7 +74,15 @@
           intermediate-inputs (range 1 (inc inputs))
           nature-types (range 1 (inc resources))
           labor-types (range 1 (inc labors))
-          ccs (create-ccs 100 10)
-])
-  )  
-)
+          ccs (create-ccs 100 10 finals)
+          worker-councils 80 ; number of worker councils
+          ]
+      (create-wcs worker-councils final-goods 0)
+      (create-wcs worker-councils intermediate-inputs 1)
+    )))
+
+
+; TODO: confused about input-count in 362 and 363 - help!
+
+; https://github.com/msszczep/pequod2/blob/master/pequod2.nlogo
+; http://ccl.northwestern.edu/netlogo/docs/
